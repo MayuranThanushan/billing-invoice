ab484957c5c03daf81b24b530affda5505897235
Mayuran Thanushan <mayuranthanushan@gmail.com>
2024-08-05 21:01:53 +0530

    Modified summary view

--- src/components/ReceiptPreview.js
+++ src/components/ReceiptPreview.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const ReceiptPreview = ({ products, totals, deleteProduct }) => {
+const ReceiptPreview = ({ products, totals, deleteProduct, handlePrint}) => {
   return (
     <div>
       <hr />
-      <table>
+      <div className='output-box'>
+      <table className='table'>
         <thead>
           <tr>
             <th>Product</th>
             <th>Quantity</th>
             <th>Unit Price</th>
             <th>Total Price</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -19,7 +21,7 @@ const ReceiptPreview = ({ products, totals, deleteProduct }) => {
               <td>{product.name}</td>
               <td>{product.quantity}</td>
               <td>{product.unitPrice}</td>
-              <td>{product.quantity * product.unitPrice}</td>
+              <td>{(product.quantity * product.unitPrice).toFixed(2)}</td>
               <td>
               <button onClick={() => deleteProduct(index)}>Delete</button>
               </td>
@@ -27,13 +29,19 @@ const ReceiptPreview = ({ products, totals, deleteProduct }) => {
           ))}
         </tbody>
       </table>
-      <div>
-        <p>Subtotal: {totals.subtotal}</p>
-        <p>Discount: {totals.discount}</p>
-        <p>Tax: {totals.tax}</p>
-        <p>Shipping: {totals.shipping}</p>
-        <p>Total: {totals.total}</p>
+      <div className='summary'>
+        <h3>Summary</h3>
+        <div className='summary-item'>
+        <p>Total <span className='value'>${totals.subtotal}</span></p>
+        <p>Discount <span className='value'>${totals.discount}</span></p>
+        <p>Tax <span className='value'>${totals.tax}</span></p>
+        <p>Shipping <span className='value'>${totals.shipping}</span></p>
+        <hr />
+        <p className='total'>SubTotal <span className='value'>${totals.total}</span></p>
+        </div>
+        <div className='btn'><button onClick={handlePrint}>Print</button></div>
       </div>
+    </div>
     </div>
   );
 };
